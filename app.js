6ef89685fe7c88a4f227acbdefa72dda17ea0ec0
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-21 22:00:50 +0100

    commit

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const port = 8080;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.get('/',(req, res)=>{
     res.render('index');
+})
+app.get('/gameplay',(req, res)=>{
+    res.render('game_play.ejs');
 })
 
 app.listen(port,()=>{
